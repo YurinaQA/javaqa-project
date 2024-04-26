ccc5cbc7597987f00f66c4ea7c448fd3db96e8ce
Alexander Khyrbu <rjrednaxela@vk.ru>
2024-04-26 22:09:56 +0300

    Fix test

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -16,7 +16,7 @@ public class SavingAccountTest {
 
         account.add(3_000);
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+        Assertions.assertEquals(5_000, account.getBalance());
     }
     @Test
     public void shouldAddZeroThanMaxBalance() {
@@ -45,6 +45,81 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000, account.getBalance());
 
+    }
+
+    @Test
+    public void shouldAddNegativeThanMaxBalance1() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(9_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+
+    }
+
+    @Test
+    public void minBalanceEqualZero() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                0,
+                10_000,
+                5
+        );
+
+        Assertions.assertEquals(0, account.getMinBalance());
+    }
+
+    @Test
+    public void maxBalanceEqualZero() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                0,
+                0,
+                5
+        );
+
+        Assertions.assertEquals(0, account.getMaxBalance());
+    }
+
+    @Test //0-ой начальный балланс
+    public void initialBalanceEqualZero() {
+        SavingAccount account = new SavingAccount(
+                0,
+                1_000,
+                10_000,
+                5
+        );
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test //Проверка 0-ой проц. ставки
+    public void rateEqualZero() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                0
+        );
+
+        Assertions.assertEquals(0, account.getRate());
+    }
+
+    @Test //Проверка начального отрицательного баланса
+    public void initialBalanceNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(
+                    -5_000,
+                    1_000,
+                    10_000,
+                    5
+            );
+        });
     }
 
 }
